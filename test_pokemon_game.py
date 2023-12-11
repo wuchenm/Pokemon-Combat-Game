9ddaf327f5e02d0648f2c247cda178a9641a3ef2
Mateo <mateowuchen1006@gmail.com>
2023-12-11 01:10:38 -0500

    More tests

--- test_pokemon_game.py
+++ test_pokemon_game.py
@@ -1,6 +1,8 @@
 import unittest
-from unittest.mock import patch
-from PokemonCombat import Move, Pokemon
+from unittest import TestCase, mock
+import pygame
+from unittest.mock import patch, Mock
+from PokemonCombat import Move, Pokemon, APIManager, FirePokemon, WaterPokemon, GrassPokemon
 
 class TestMove(unittest.TestCase):
     @patch('PokemonCombat.APIManager.get_move_data')
@@ -19,6 +21,65 @@ class TestMove(unittest.TestCase):
         self.assertEqual(move.name, "tackle")
         self.assertEqual(move.power, 40)
         self.assertEqual(move.type, "normal")
+
+class TestPokemon(unittest.TestCase):
+    '''
+    Perform_attack method is not tested due to the complexity:
+    - a lot of interaction with pokemon and move instances
+    - involves random elements and conveys side effects (printing in console, updating display...)
+    '''
+    def setUp(self):
+        # Initialize Pygame and its display module
+        pygame.init()
+        pygame.display.set_mode((1, 1))  # Minimal display size
+
+        # Create a test Pokemon instance for testing
+        self.pokemon = Pokemon('Pikachu', 10, 100, 100)
+        self.attacker = Pokemon('Charmander', 10, 100, 100)
+        self.defender = Pokemon('Squirtle', 10, 100, 100)
+
+    def test_initialization(self):
+        self.assertEqual(self.pokemon.name, 'Pikachu')
+        self.assertEqual(self.pokemon.level, 10)
+
+    def test_take_damage(self):
+        initial_hp = self.pokemon.current_hp
+        self.pokemon.take_damage(10)
+        self.assertEqual(self.pokemon.current_hp, initial_hp - 10)
+
+    def test_use_potion(self):
+        self.pokemon.current_hp -= 20  # Simulate damage
+        initial_potions = self.pokemon.num_potions
+        self.pokemon.use_potion()
+        self.assertEqual(self.pokemon.current_hp, self.pokemon.max_hp)
+        self.assertEqual(self.pokemon.num_potions, initial_potions - 1)
+
+    def test_set_moves(self):
+        self.pokemon.set_moves()
+        self.assertIsNotNone(self.pokemon.moves)
+        self.assertTrue(len(self.pokemon.moves) > 0)
+
+class TestAPIManager(unittest.TestCase):
+
+    @patch('requests.get')
+    def test_get_pokemon_data(self, mock_get):
+        # Mock a API response
+        mock_get.return_value = unittest.mock.Mock(status_code=200, json=lambda: {"name": "pikachu"})
+        result = APIManager.get_pokemon_data("pikachu")
+
+        self.assertIsNotNone(result)
+        self.assertEqual(result['name'], "pikachu")
+
+    @patch('requests.get')
+    def test_get_move_data_success(self, mock_get):
+        # Mock a API response for move data
+        mock_get.return_value = unittest.mock.Mock(status_code=200, json=lambda: {"name": "tackle", "power": 40})
+        result = APIManager.get_move_data("http://example.com/move/tackle")
+
+        self.assertIsNotNone(result)
+        self.assertEqual(result['name'], "tackle")
+        self.assertEqual(result['power'], 40)
+
 
 if __name__ == '__main__':
     unittest.main()
